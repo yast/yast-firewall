a6f72d20d39070331ce2ce4e74421f0bd24c60a7
Martin Vidner <mvidner@suse.cz>
2018-08-28 18:10:39 +0200

    WIP new UI
        rake run[firewall_new]

---
+++ src/lib/y2firewall/clients/firewall_new.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2018 SUSE LLC
+#
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact SUSE.
+#
+# To contact SUSE about this file by physical or electronic mail, you may find
+# current contact information at www.suse.com.
+# ------------------------------------------------------------------------------
+
+require "yast"
+require "yast/i18n"
+require "yast2/popup"
+require "cwm/tree_pager"
+require "y2firewall/dialogs/main"
+
+module Y2Firewall
+  # YaST "clients" are the CLI entry points
+  module Clients
+    # The entry point for starting the firewall UI.
+    class FirewallNew
+      include Yast::I18n
+      include Yast::Logger
+
+      def initialize
+        textdomain "firewall"
+      end
+
+      # Runs the client
+      def run
+        Dialogs::Main.new.run
+      end
+    end
+  end
+end
