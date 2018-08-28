71b918de1df14f322b803cd33227dfe1cfb9b1e7
Martin Vidner <mvidner@suse.cz>
2018-08-28 18:10:39 +0200

    WIP new UI
        rake run[firewall_new]

---
+++ src/lib/y2firewall/dialogs/main.rb
@@ -0,0 +1,72 @@
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2018 SUSE LLC
+#
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact SUSE.
+#
+# To contact SUSE about this file by physical or electronic mail, you may find
+# current contact information at www.suse.com.
+# ------------------------------------------------------------------------------
+
+require "yast"
+require "cwm/dialog"
+require "y2firewall/widgets/overview"
+
+Yast.import "Label"
+
+module Y2Firewall
+  module Dialogs
+    # Main entry point to Firewall showing tree pager with all content
+    class Main < CWM::Dialog
+      def initialize
+        textdomain "firewall"
+      end
+
+      def title
+        _("Firewall")
+      end
+
+      def contents
+        MarginBox(
+          0.5,
+          0.5,
+          Widgets::OverviewTreePager.new
+        )
+      end
+
+      def back_button
+        # do not show back button when running on running system. See CWM::Dialog.back_button
+        ""
+      end
+
+      def next_button
+        Yast::Label.AcceptButton
+      end
+
+      def abort_button
+        Yast::Label.AbortButton
+      end
+
+      # @return [Boolean] it aborts if returns true
+      def abort_handler
+        true
+      end
+
+      # @return [Boolean] it goes back if returns true
+      def back_handler
+        true
+      end
+    end
+  end
+end
---
+++ src/lib/y2firewall/widgets/pages.rb
@@ -0,0 +1,85 @@
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2018 SUSE LLC
+#
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact SUSE.
+#
+# To contact SUSE about this file by physical or electronic mail, you may find
+# current contact information at www.suse.com.
+# ------------------------------------------------------------------------------
+
+module Y2Firewall
+  module Widgets
+    module Pages
+      class Startup < CWM::Page
+         # Constructor
+        #
+        # @param pager [CWM::TreePager]
+        def initialize(pager)
+          textdomain "firewall"
+        end
+
+        # @macro seeAbstractWidget
+        def label
+          "FIXME Startup"
+        end
+
+        # @macro seeCustomWidget
+        def contents
+          Label("HELLO 1")
+        end
+      end
+
+      class AllowedServices < CWM::Page
+        # Constructor
+        #
+        # @param pager [CWM::TreePager]
+        def initialize(pager)
+          textdomain "firewall"
+        end
+
+        # @macro seeAbstractWidget
+        def label
+          "FIXME Services"
+        end
+
+        # @macro seeCustomWidget
+        def contents
+          Label("HELLO 2")
+        end
+      end
+
+      class AllowedServicesForZone < CWM::Page
+        # Constructor
+        #
+        # @param pager [CWM::TreePager]
+        def initialize(zone, pager)
+          textdomain "firewall"
+          @zone = zone
+          self.widget_id = "asz:" + zone
+        end
+
+        # @macro seeAbstractWidget
+        def label
+          "FIXME Services for #{@zone}"
+        end
+
+        # @macro seeCustomWidget
+        def contents
+          Label("HELLO #{@zone}")
+        end
+      end
+    end
+  end
+end
