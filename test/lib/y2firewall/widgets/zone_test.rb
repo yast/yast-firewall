c94908af6d8a525986f40b93fd09e31c381b6fe7
Josef Reidinger <jreidinger@suse.cz>
2019-01-10 14:42:57 +0100

    Add basic tests for zone widgets

---
+++ test/lib/y2firewall/widgets/zone_test.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env rspec
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../../test_helper"
+
+require "cwm/rspec"
+require "y2firewall/widgets/zone"
+
+describe Y2Firewall::Dialogs::NameWidget do
+  subject { described_class.new(double(name: "test")) }
+
+  include_examples "CWM::AbstractWidget"
+end
+
+describe Y2Firewall::Dialogs::ShortWidget do
+  subject { described_class.new(double(short: "test")) }
+
+  include_examples "CWM::AbstractWidget"
+end
+
+describe Y2Firewall::Dialogs::DescriptionWidget do
+  subject { described_class.new(double(description: "test")) }
+
+  include_examples "CWM::AbstractWidget"
+end
+
+describe Y2Firewall::Dialogs::TargetWidget do
+  subject { described_class.new(double(target: "default")) }
+
+  include_examples "CWM::ComboBox"
+end
+
+describe Y2Firewall::Dialogs::MasqueradeWidget do
+  subject { described_class.new(double(masquerade: false)) }
+
+  include_examples "CWM::CheckBox"
+end
