2e4efb82b7396cb78bba71bae28a4ae444a6bd1b
Stefan Schubert <schubi@suse.de>
2020-05-07 15:07:35 +0200

    comments

--- src/lib/y2firewall/autoinst_profile/firewall_section.rb
+++ src/lib/y2firewall/autoinst_profile/firewall_section.rb
@@ -22,7 +22,7 @@ require "installation/autoinst_profile/section_with_attributes"
 module Y2Firewall
   module AutoinstProfile
     # This class represents an AutoYaST \<firewall> section
-
+    #
     class FirewallSection < ::Installation::AutoinstProfile::SectionWithAttributes
       # Creates an instance based on the profile representation used by the AutoYaST modules
       # (hash with nested hashes and arrays).
